42116a919c7ee478cd430668869011473575a73d
belbogarin <bbogarin96@gmail.com>
2023-03-01 10:52:14 -0300

    primera pagina

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const $portfolio =document.querySelector ("sec-portfolio-js"
+)
+$portfolio .addEventListener ("click",(e)=> {
+    
+    if  (e.target.classlist.contains ('img-btn-modal-js')) {
+       
+    }
+});
